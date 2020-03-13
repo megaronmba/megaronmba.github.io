7a4e7ee7935998555f19fe185e295ee85eaf73f2
megaronmba <60791331+megaronmba@users.noreply.github.com>
2020-03-13 10:32:05 -0300

    Update mb42.psicoTest.js

--- intro/js3/mb42.psicoTest.js
+++ intro/js3/mb42.psicoTest.js
@@ -10,30 +10,10 @@ var persona = function (codes, id, name, page) {
 };
 
 var personas = {
-    David: new persona(['INTP', 'ISTP', 'INTJ', 'INFJ'], '1.1.1. David; 2.Sophia; 3.William; 4.Laura', 'David Laura, the Innovator', 'http://www.MBA4Heroes.com'),
-    Sophia: new persona(['ISTJ', 'ISFJ', 'ENTJ'], '1.1. Sophia; 2.William; 3.Laura; 4.David', 'Sophia David, the Conductor', 'http://www.MBA4Heroes.com'),
-    William: new persona(['ESTJ', 'ESTP', 'ESFP', 'ISFP'], 'Result: William.
-
- 
-
-4 Heroes - Profile
-
- 
-
-David, the Innovator. Among so many skills, I distinguish myself by offering solutions to highly complex problems. I can relate knowledge between apparently unrelated areas. Post MBA career plan: Big Tech Companies / Innovative Companies;
-
- 
-
-Sophia, the Strategist. Among so many skills, I distinguish myself by offering solutions to problems that require rigorous analysis. I can interact well in a group. Post MBA career plan: Management Consulting / Strategic Planning in Large Corporations;
-
- 
-
-William, the Doer. Among so many skills, I distinguish myself by being able to combine analysis with implementation. I not only propose solutions, but also press the trigger for execution. Post MBA career plan: Private Equity Fund / Entrepreneurial Companies;
-
- 
-
-Laura, the Mentor. Among so many skills, I distinguish myself by valuing the human element in a group. I can bring people together around one goal. Post MBA career plan: Entrepreneurship (Startup) / Public Private Frontiers.', 'William David, the Implementer', 'http://www.MBA4Heroes.com'),
-    Laura: new persona(['ENTP', 'ENFP', 'INFP', 'ENFJ', 'ESFJ'], '1.1. Laura; 2.David; 3.Sophia; 4.William', 'Laura David, the Mentor', 'http://www.MBA4Heroes.com')
+    David: new persona(['INTP', 'ISTP', 'INTJ', 'INFJ'], '1.2.David; 2.Sophia; 3.William; 4.Laura', 'David Laura, the Innovator', 'http://www.MBA4Heroes.com'),
+    Sophia: new persona(['ISTJ', 'ISFJ', 'ENTJ'], '1.2.Sophia; 2.William; 3.Laura; 4.David', 'Sophia David, the Conductor', 'http://www.MBA4Heroes.com'),
+    William: new persona(['ESTJ', 'ESTP', 'ESFP', 'ISFP'], '1.2.William; 2.Laura, the Mentor. Among so many skills, I distinguish myself by valuing the human element in a group. I can bring people together around one goal. Post MBA career plan: Entrepreneurship (Startup) / Public Private Frontiers.', 'William David, the Implementer', 'http://www.MBA4Heroes.com'),
+    Laura: new persona(['ENTP', 'ENFP', 'INFP', 'ENFJ', 'ESFJ'], '1.2.Laura; 2.David; 3.Sophia; 4.William', 'Laura David, the Mentor', 'http://www.MBA4Heroes.com')
 };
 
 var optionTypes = {
